d377bca012a0362546b342ec43d3c61849321f61
dineshADK2057 <dineshADK2057@users.noreply.github.com>
2024-09-23 11:08:27 +0545

    Initial commit

---
+++ JavaScript/16/16.js
@@ -0,0 +1,102 @@
+// const btn = document.getElementById("btn");
+// const text = document.getElementById("text");
+
+// btn.addEventListener("click", () => {
+//   text.style.opacity = 1;
+// });
+
+// variables
+
+// var (global scope) a = 23;
+
+// a = 45;
+
+// const (block scope) a = 64;
+
+// let (Block scope) a = 34;
+// a = 31
+
+
+// data types
+
+// number 
+
+// let a = 31; 
+
+// strings
+// let b = "hell0"
+
+// a = "31"
+
+// boolean (true, false)
+
+// a = 0;
+
+// arithematic operators: +, -, /, *, ++, --
+
+// let s=12;
+// s++ // s = s+1
+
+// s-- //s = s-1
+
+// logical: AND(&&), OR(||)
+// if(a==false){
+//     console.log("ture");
+// }
+// else{
+//     console.log("false");
+// }
+
+
+// if(conditions){
+//     "statements"
+// }
+// else if (condition){
+//     "statements"
+// }
+// .
+// .
+// .
+// .
+// .
+// else{
+
+// }
+
+// comparison: <, >, <=, >=, ==, ===
+
+// ternary operator: (condition) ? "statements if ture" : "statement if false"
+
+// (a===false) ? console.log("true") : (condition) ? "expression" : "expression";
+
+
+// console.log(s);
+
+
+let a = 3;
+
+if (a==1) {
+    console.log("Sunday");
+}
+else if (a==2) {
+    console.log("Monday");
+}
+else if (a==3) {
+    console.log("Tuesday");
+}
+else if (a==4) {
+    console.log("Wednesday");
+}
+else if (a==5) {
+    console.log("thursday");
+}
+else if (a==6) {
+    console.log("Friday");
+}
+
+else {
+    console.log("Saturday")
+}
+
+
+
